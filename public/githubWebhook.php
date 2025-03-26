7bfd57ac8ca723e729ecfb2b42490d9609a784c3
Fabien Roy <120800630+fabry44@users.noreply.github.com>
2025-03-26 17:09:47 +0100

    feat: update GitHub webhook script to use correct path for Portfolio-API deployment

--- public/githubWebhook.php
+++ public/githubWebhook.php
@@ -33,9 +33,9 @@ if (!hash_equals($signature, $headers['X-Hub-Signature-256'])) {
 
 // Si vérification OK, alors exécuter le déploiement
 $output = [];
-exec('cd /chemin/exact/vers/Portfolio-API && git pull origin dev 2>&1', $output);
-exec('cd /chemin/exact/vers/Portfolio-API && php composer.phar install --no-dev --optimize-autoloader 2>&1', $output);
-exec('cd /chemin/exact/vers/Portfolio-API && php bin/console doctrine:migrations:migrate -n 2>&1', $output);
-exec('cd /chemin/exact/vers/Portfolio-API && php bin/console cache:clear 2>&1', $output);
+exec('cd /home/u120012058/domains/dashboard.fabien-roy.fr/public_html/Portfolio-API && git pull origin dev 2>&1', $output);
+exec('cd /home/u120012058/domains/dashboard.fabien-roy.fr/public_html/Portfolio-API && php composer.phar install --no-dev --optimize-autoloader 2>&1', $output);
+exec('cd /home/u120012058/domains/dashboard.fabien-roy.fr/public_html/Portfolio-API && php bin/console doctrine:migrations:migrate -n 2>&1', $output);
+exec('cd /home/u120012058/domains/dashboard.fabien-roy.fr/public_html/Portfolio-API && php bin/console cache:clear 2>&1', $output);
 
 echo implode("\n", $output);
