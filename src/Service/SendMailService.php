20bf79acccc0b4447189dfade820c63c6d693cd7
Fabien Roy <120800630+fabry44@users.noreply.github.com>
2025-03-27 17:38:25 +0100

    feat: add logging of email context for debugging in SendMailService

--- src/Service/SendMailService.php
+++ src/Service/SendMailService.php
@@ -32,7 +32,9 @@ class SendMailService
         string $template,
         array $context
     ): void
-    {
+    {   
+        file_put_contents(__DIR__ . '/../../var/log/debug_mail_context.log', print_r($context, true));
+        
         //On crée le mail
         $email = (new TemplatedEmail())
             ->from($from)
@@ -41,8 +43,8 @@ class SendMailService
             ->htmlTemplate("mail/$template.html.twig")
             ->context($context);
 
-        file_put_contents(__DIR__ . '/../../var/log/debug_mail_context.log', print_r($context, true));
         
+
         // On envoie le mail
         $this->mailer->send($email);
     }
